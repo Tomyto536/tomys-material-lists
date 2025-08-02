7ffe7c843217c6d95229e44022cd5988efcfde1e
Tomyto536 <thomas.langdeau@gmail.com>
2025-08-02 18:30:26 -0400

    Open basic screen with custom keybind

--- src/client/java/me/tomyto/tomysMarterialListsJava/client/TomysMarterialListsJavaClient.java
+++ src/client/java/me/tomyto/tomysMarterialListsJava/client/TomysMarterialListsJavaClient.java
@@ -1,10 +1,71 @@
 package me.tomyto.tomysMarterialListsJava.client;
 
+import io.wispforest.owo.ui.base.BaseOwoScreen;
+import io.wispforest.owo.ui.container.Containers;
+import io.wispforest.owo.ui.container.FlowLayout;
+import io.wispforest.owo.ui.core.*;
 import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
+import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.util.InputUtil;
+import org.jetbrains.annotations.NotNull;
+import io.wispforest.owo.ui.core.OwoUIAdapter;
+
+
+import org.lwjgl.glfw.GLFW;
+import net.minecraft.client.option.KeyBinding;
+
+
+import java.awt.*;
 
 public class TomysMarterialListsJavaClient implements ClientModInitializer {
 
+    public static KeyBinding openScreenKeyBind;
+
+
     @Override
     public void onInitializeClient() {
+        // Register a keybind for LEFT CONTROL
+        openScreenKeyBind = KeyBindingHelper.registerKeyBinding(new KeyBinding(
+                "Open Material List", // translation key
+                InputUtil.Type.KEYSYM,
+                GLFW.GLFW_KEY_LEFT_CONTROL,
+                "Tomy's Material List" // category shown in Controls menu
+        ));
+
+        // Register the event that checks for key presses every client tick
+        ClientTickEvents.END_CLIENT_TICK.register(client -> {
+            while (openScreenKeyBind.wasPressed()) {
+                if (client.currentScreen == null ) {
+                    client.setScreen(new MaterialListScreen());
+                }
+            }
+        });
+
     }
+
+    public class MaterialListScreen extends BaseOwoScreen<io.wispforest.owo.ui.container.FlowLayout> {
+
+        @Override
+        protected @NotNull OwoUIAdapter<io.wispforest.owo.ui.container.FlowLayout> createAdapter() {
+            return OwoUIAdapter.create(this, Containers::verticalFlow);
+        }
+
+        @Override
+        protected void build(FlowLayout rootComponent) {
+            rootComponent.surface(Surface.VANILLA_TRANSLUCENT);
+        }
+
+        @Override
+        public boolean keyPressed(int keyCode, int scanCode, int modifiers) {
+            if (openScreenKeyBind.matchesKey(keyCode, scanCode)) {
+                MinecraftClient.getInstance().setScreen(null);
+                return true;
+            }
+            return super.keyPressed(keyCode, scanCode, modifiers);
+        }
+
+    }
+
 }
--- src/main/java/me/tomyto/tomysMarterialListsJava/TomysMarterialListsJava.java
+++ src/main/java/me/tomyto/tomysMarterialListsJava/TomysMarterialListsJava.java
@@ -1,10 +1,16 @@
 package me.tomyto.tomysMarterialListsJava;
 
+import io.wispforest.owo.ui.container.Containers;
 import net.fabricmc.api.ModInitializer;
+
+
+
+import java.awt.*;
 
 public class TomysMarterialListsJava implements ModInitializer {
 
     @Override
     public void onInitialize() {
     }
+
 }
